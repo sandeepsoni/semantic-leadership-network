676e3192b9c8262f92d9ab1d1f286141b2d24fe2
Sandeep Soni <soni.sandeepb@gmail.com>
2022-03-31 07:36:48 -0700

    Update pipeline_temporal.sh

--- scripts/pipeline_temporal.sh
+++ scripts/pipeline_temporal.sh
@@ -4,16 +4,16 @@
 
 ## Find the nearest neighbors
 # Note that this step requires a few hours
-#parallel python near_neighbors.py --dir-path ../data/aa_temp_grouped --embeddings-file out.embeddings --near-neighbors-file {1}.neighbors.pkl --facet-name {1} --nearest 25 ::: MAIN T0 T1 T2 T3 T4 T5 T6 T7 T8 T9
+parallel python near_neighbors.py --dir-path ../data/aa_temp_grouped --embeddings-file out.embeddings --near-neighbors-file {1}.neighbors.pkl --facet-name {1} --nearest 25 ::: MAIN T0 T1 T2 T3 T4 T5 T6 T7 T8 T9
 
 ## Find the semantic changes according to the "local" metric for change
-#python local_dynamic_ranks.py --dir-path ../data/aa_temp_grouped --embeddings-file out.embeddings --scores-file local.scores
+python local_dynamic_ranks.py --dir-path ../data/aa_temp_grouped --embeddings-file out.embeddings --scores-file local.scores
 
 ## Find all the frequency files
-#python get_frequency_stats.py --dir-path ../data/aa_temp_grouped --data-file data.txt --word-freq-file words.freq --doc-freq-file docs.freq --period-freq-file periods.freq --source-freq-file sources.freq --periods 10
+python get_frequency_stats.py --dir-path ../data/aa_temp_grouped --data-file data.txt --word-freq-file words.freq --doc-freq-file docs.freq --period-freq-file periods.freq --source-freq-file sources.freq --periods 10
 
 ## Name identification filter (to be run separately and just once)
-#python name_identification.py --dir-path ../data/aa_temp_grouped --scores-file local.scores --embeddings-file out.embeddings --names-file names.ignore --casestats-file ../annotations/wordcase.stats --annotations-file ../annotations/names_locations.csv
+python name_identification.py --dir-path ../data/aa_temp_grouped --scores-file local.scores --embeddings-file out.embeddings --names-file names.ignore --casestats-file ../annotations/wordcase.stats --annotations-file ../annotations/names_locations.csv
 
 ## Create all other filters
 python create_filters.py --dir-path ../data/aa_temp_grouped --periods-freq-file periods.freq --sources-freq-file sources.freq --scores-file local.scores
